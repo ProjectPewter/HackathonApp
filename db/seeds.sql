0a9f234f785e24fe4be79feac6decc0a78c604bf
lblueter <logan11023@yahoo.com>
2018-11-20 16:57:00 -0500

    upgrade seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
 USE users_db;
 
 INSERT INTO ideas (name, details, tech, difficulty)
-VALUES ("Find a Pal!", "Helps you to find a pal in these tough times.", "HTML, CSS, JavaScript", 2);
+VALUES ("FriendFinder", "Helps you to find a friend using a multiple choice survey to find compatibility.", "HTML, CSS, JavaScript", 2);
 
 INSERT INTO ideas (name, details, tech, difficulty)
-VALUES ("Find a Pet!", "Helps you to find a pet in these tough times.", "HTML, CSS, JavaScript", 1);
+VALUES ("Find a Pet!", "Look through pictures of different pets to find what kind of pet you want!", "HTML, CSS, JavaScript", 1);
 
 INSERT INTO ideas (name, details, tech, difficulty)
 VALUES ("Dog Adoption Matcher", "Dating app between owner and potential dog. Pull dogs from local shelters and match to owners profile.", "API, SQL, Node.js, Express", 3);
